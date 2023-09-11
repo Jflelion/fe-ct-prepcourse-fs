e00aca3a6b39f65359974827769761220e518229
Juan Felipe <juanfelipelelion@gmail.com>
2023-09-11 16:19:16 -0500

    Agregado nuevo componente de la tarea

--- M09 Ejercicios Extra/Ejercicios Extra.js
+++ M09 Ejercicios Extra/Ejercicios Extra.js
@@ -3,9 +3,16 @@
 function deObjetoAarray(objeto) {
    // Recibes un objeto. Tendrás que crear un arreglo de arreglos.
    // Cada elemento del arreglo padre será un nuevo arreglo que contendrá dos elementos.
-   // Estos elementos debe ser cada par clave:valor del objeto recibido.
+   // Estos elementos deben ser cada par clave:valor del objeto recibido.
    // [EJEMPLO]: {D: 1, B: 2, C: 3} ---> [['D', 1], ['B', 2], ['C', 3]].
    // Tu código:
+   const arrayDePares = [];
+
+   for (const clave in objeto) {
+      arrayDePares.push([clave, objeto[clave]]);
+   }
+
+   return arrayDePares;
 }
 
 function numberOfCharacters(string) {
@@ -14,6 +21,20 @@ function numberOfCharacters(string) {
    // Las letras deben estar en orden alfabético.
    // [EJEMPLO]: "adsjfdsfsfjsdjfhacabcsbajda" ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 }
    // Tu código:
+   const charCount = {};
+
+   for (let i = 0; i < string.length; i++) {
+      const char = string.charAt(i);
+      charCount[char] = (charCount[char] || 0) + 1;
+   }
+
+   // Ordenar el objeto por clave alfabéticamente
+   const sortedCharCount = {};
+   Object.keys(charCount).sort().forEach((key) => {
+      sortedCharCount[key] = charCount[key];
+   });
+
+   return sortedCharCount;
 }
 
 function capToFront(string) {
@@ -22,25 +43,49 @@ function capToFront(string) {
    // Retornar el string.
    // [EJEMPLO]: soyHENRY ---> HENRYsoy
    // Tu código:
+   const uppercaseChars = [];
+   const lowercaseChars = [];
+
+   for (let i = 0; i < string.length; i++) {
+      if (string[i] === string[i].toUpperCase()) {
+         uppercaseChars.push(string[i]);
+      } else {
+         lowercaseChars.push(string[i]);
+      }
+   }
+
+   return uppercaseChars.join('') + lowercaseChars.join('');
 }
 
 function asAmirror(frase) {
    // Recibes una frase. Tu tarea es retornar un nuevo string en el que el orden de las palabras sea el mismo.
-   // La diferencia es que cada palabra estará escrita al inverso.
+   // La diferencia es que cada palabra estará escrita al revés.
    // [EJEMPLO]: "The Henry Challenge is close!"  ---> "ehT yrneH egnellahC si !esolc"
    // Tu código:
+   const palabras = frase.split(' ');
+   const palabrasAlReves = palabras.map((palabra) => palabra.split('').reverse().join(''));
+   return palabrasAlReves.join(' ');
 }
 
 function capicua(numero) {
-   // Si el número que recibes es capicúa debes retornar el string: "Es capicua".
-   // Caso contrario: "No es capicua".
+   // Si el número que recibes es capicúa debes retornar el string: "Es capicúa".
+   // Caso contrario: "No es capicúa".
    // Tu código:
+   const numeroStr = numero.toString();
+   const numeroAlReves = numeroStr.split('').reverse().join('');
+
+   if (numeroStr === numeroAlReves) {
+      return "Es capicúa";
+   } else {
+      return "No es capicúa";
+   }
 }
 
 function deleteAbc(string) {
    // Tu tarea es eliminar las letras "a", "b" y "c" del string recibido.
    // Retorna el string sin estas letras.
    // Tu código:
+   return string.replace(/[abc]/g, '');
 }
 
 function sortArray(arrayOfStrings) {
@@ -49,6 +94,7 @@ function sortArray(arrayOfStrings) {
    // de la longitud de cada string.
    // [EJEMPLO]: ["You", "are", "beautiful", "looking"]  ---> [“You", "are", "looking", "beautiful"]
    // Tu código:
+   return arrayOfStrings.sort((a, b) => a.length - b.length);
 }
 
 function buscoInterseccion(array1, array2) {
@@ -58,6 +104,15 @@ function buscoInterseccion(array1, array2) {
    // Si no tienen elementos en común, retornar un arreglo vacío.
    // [PISTA]: los arreglos no necesariamente tienen la misma longitud.
    // Tu código:
+   const interseccion = [];
+
+   for (const num1 of array1) {
+      if (array2.includes(num1) && !interseccion.includes(num1)) {
+         interseccion.push(num1);
+      }
+   }
+
+   return interseccion;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
